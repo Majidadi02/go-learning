9019e6bcd5f9a757e4218a4a47286df3e4160c22
mus'ab majidadi <mahmudmusab123@gmail.com>
2026-07-19 01:15:26 -0700

    Complete Challenge 001: Hello Go

---
+++ level-01-fundamentals/001-hello-go/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("====================================")
+	fmt.Println("GO LEARNING JOURNEY")
+	fmt.Println("====================================")
+
+	fmt.Println("Name       : Majidadi")
+	fmt.Println("University : Ahmadu Bello University")
+	fmt.Println("Course     : Architecture")
+	fmt.Println("Language   : Go")
+	fmt.Println("Goal       : Become a Bitcoin Developer")
+
+	fmt.Println("Welcome to my Go programming journey!")
+}
